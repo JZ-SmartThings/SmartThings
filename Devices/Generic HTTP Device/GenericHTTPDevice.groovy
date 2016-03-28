b252369f6c35f5fe48d5addb79372c7743a12801
JZ-SmartThings <jzelyeny@gmail.com>
2016-03-27 21:38:03 -0700

    Fixed indenting in the groovy file

--- Devices/Generic HTTP Device/GenericHTTPDevice.groovy
+++ Devices/Generic HTTP Device/GenericHTTPDevice.groovy
@@ -41,7 +41,7 @@ metadata {
 		input("DevicePath", "string", title:"URL Path", description: "Rest of the URL, include forward slash.", displayDuringSetup: true)
 		input(name: "DevicePostGet", type: "enum", title: "POST or GET", options: ["POST","GET"], required: true, displayDuringSetup: true)
 		input("DeviceBodyText", "string", title:'Body Content', description: 'Type in "GateTrigger=" for PHP POST', displayDuringSetup: true)
-        section() {
+		section() {
 			input("HTTPAuth", "bool", title:"Requires User Auth?", description: "Choose if the HTTP requires basic authentication", defaultValue: false, required: true, displayDuringSetup: true)
 			input("HTTPUser", "string", title:"HTTP User", description: "Enter your basic username", required: false, displayDuringSetup: true)
 			input("HTTPPassword", "string", title:"HTTP Password", description: "Enter your basic password", required: false, displayDuringSetup: true)
@@ -52,33 +52,33 @@ metadata {
 	}
 
 	tiles {
-        valueTile("lastTriggered", "device.lastTriggered", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: 'Last triggered:\n${currentValue}', backgroundColor:"#ffffff")
-        }
-        standardTile("DeviceTrigger", "device.triggerswitch", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, decoration: "flat") {
+		valueTile("lastTriggered", "device.lastTriggered", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: 'Last triggered:\n${currentValue}', backgroundColor:"#ffffff")
+		}
+		standardTile("DeviceTrigger", "device.triggerswitch", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, decoration: "flat") {
 			state "default", label:'GATE' , action: "DeviceTrigger", icon: "st.Outdoor.outdoor22", backgroundColor:"#53a7c0", nextState: "triggerrunning"
             state "triggerrunning", label: 'OPENING', action: "ResetTiles", icon: "st.Outdoor.outdoor22", backgroundColor: "#FF6600", nextState: "default"
 		}
-        valueTile("testTriggered", "device.testTriggered", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: 'Test triggered:\n${currentValue}', backgroundColor:"#ffffff")
-        }
-        standardTile("TestTrigger", "device.testswitch", width: 1, height: 1, decoration: "flat") {
-			state "default", label:'TEST', action: "TestTrigger", icon: "st.Office.office13", backgroundColor:"#53a7c0", nextState: "testrunning"
-            state "testrunning", label: 'TESTING', action: "ResetTiles", icon: "st.Office.office13", backgroundColor: "#FF6600", nextState: "default"
+		valueTile("testTriggered", "device.testTriggered", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: 'Test triggered:\n${currentValue}', backgroundColor:"#ffffff")
 		}
-        valueTile("cpuUsage", "device.cpuUsage", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: '${currentValue}', backgroundColor:"#ffffff")
-        }
-        valueTile("spaceUsed", "device.spaceUsed", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: '${currentValue}', backgroundColor:"#ffffff")
-        }
-        valueTile("upTime", "device.upTime", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: '${currentValue}', backgroundColor:"#ffffff")
-        }
-        valueTile("cpuTemp", "device.cpuTemp", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
-            state("default", label: '${currentValue}', backgroundColor:"#ffffff")
-        }
-        standardTile("RebootNow", "device.switch", width: 1, height: 1, decoration: "flat") {
+		standardTile("TestTrigger", "device.testswitch", width: 1, height: 1, decoration: "flat") {
+			state "default", label:'TEST', action: "TestTrigger", icon: "st.Office.office13", backgroundColor:"#53a7c0", nextState: "testrunning"
+			state "testrunning", label: 'TESTING', action: "ResetTiles", icon: "st.Office.office13", backgroundColor: "#FF6600", nextState: "default"
+		}
+		valueTile("cpuUsage", "device.cpuUsage", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: '${currentValue}', backgroundColor:"#ffffff")
+			}
+		valueTile("spaceUsed", "device.spaceUsed", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: '${currentValue}', backgroundColor:"#ffffff")
+		}
+		valueTile("upTime", "device.upTime", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: '${currentValue}', backgroundColor:"#ffffff")
+		}
+		valueTile("cpuTemp", "device.cpuTemp", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+			state("default", label: '${currentValue}', backgroundColor:"#ffffff")
+		}
+		standardTile("RebootNow", "device.switch", width: 1, height: 1, decoration: "flat") {
 			state "default", label:'REBOOT' , action: "RebootNow", icon: "st.Seasonal Winter.seasonal-winter-014", backgroundColor:"#ff0000"
 		}
 //		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 1, height: 1) {
@@ -91,16 +91,16 @@ metadata {
 
 def DeviceTrigger() {
 	log.debug "Gate Triggered!!!"
-    //sendEvent(name: "DeviceTrigger", value: "test", unit: "")
-    runCmd(DeviceBodyText)
+	//sendEvent(name: "DeviceTrigger", value: "test", unit: "")
+	runCmd(DeviceBodyText)
 }
 def TestTrigger() {
 	log.debug "Test Triggered!!!"
-    runCmd('Test=')
+	runCmd('Test=')
 }
 def RebootNow() {
 	log.debug "Reboot Triggered!!!"
-    runCmd('RebootNow=')
+	runCmd('RebootNow=')
 }
 def ResetTiles() {
 	//RETURN BUTTONS TO CORRECT STATE
@@ -109,57 +109,57 @@ def ResetTiles() {
 	log.debug "Resetting tiles."
 }
 def runCmd(String varCommand) {
-    def host = DeviceIP 
-    def hosthex = convertIPtoHex(host).toUpperCase()
-    def porthex = convertPortToHex(DevicePort).toUpperCase()
-    device.deviceNetworkId = "$hosthex:$porthex" 
+	def host = DeviceIP 
+	def hosthex = convertIPtoHex(host).toUpperCase()
+	def porthex = convertPortToHex(DevicePort).toUpperCase()
+	device.deviceNetworkId = "$hosthex:$porthex" 
 	def userpassascii = "${HTTPUser}:${HTTPPassword}"
 	def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
-    
-    log.debug "The device id configured is: $device.deviceNetworkId"
-    
-    def path = DevicePath
-    log.debug "path is: $path"
-    log.debug "Uses which method: $DevicePostGet"
-    def body = varCommand 
-    log.debug "body is: $body"
-    
-    def headers = [:] 
-    headers.put("HOST", "$host:$DevicePort")
+	
+	log.debug "The device id configured is: $device.deviceNetworkId"
+	
+	def path = DevicePath
+	log.debug "path is: $path"
+	log.debug "Uses which method: $DevicePostGet"
+	def body = varCommand 
+	log.debug "body is: $body"
+	
+	def headers = [:] 
+	headers.put("HOST", "$host:$DevicePort")
 	headers.put("Content-Type", "application/x-www-form-urlencoded")
-   	if (HTTPAuth) {
-        headers.put("Authorization", userpass)
-    }
+	if (HTTPAuth) {
+		headers.put("Authorization", userpass)
+	}
 
-    log.debug "The Header is $headers"
-    
-    def method = "POST"
-    try {
-    	if (DevicePostGet.toUpperCase() == "GET") {
-        	method = "GET"
-        	}
-        }
-    catch (Exception e) {
-    	settings.DevicePostGet = "POST"
-        log.debug e
-        log.debug "You must not have set the preference for the DevicePOSTGET option"
-    }
-    log.debug "The method is $method"
-    try {
-    def hubAction = new physicalgraph.device.HubAction(
-    	method: method,
-    	path: path,
+	log.debug "The Header is $headers"
+	
+	def method = "POST"
+	try {
+		if (DevicePostGet.toUpperCase() == "GET") {
+			method = "GET"
+			}
+		}
+	catch (Exception e) {
+		settings.DevicePostGet = "POST"
+		log.debug e
+		log.debug "You must not have set the preference for the DevicePOSTGET option"
+	}
+	log.debug "The method is $method"
+	try {
+	def hubAction = new physicalgraph.device.HubAction(
+		method: method,
+		path: path,
 		body: body,
-    	headers: headers
-        )
+		headers: headers
+		)
 
-    hubAction.options = [outputMsgToS3:false]
-    //log.debug hubAction
-    hubAction
-    }
-    catch (Exception e) {
-    	log.debug "Hit Exception $e on $hubAction"
-    }   
+	hubAction.options = [outputMsgToS3:false]
+	//log.debug hubAction
+	hubAction
+	}
+	catch (Exception e) {
+		log.debug "Hit Exception $e on $hubAction"
+	}   
 
 }
 
@@ -168,7 +168,7 @@ def parse(String description) {
 //    log.debug "Parsing '${description}'"
 
 	def whichTile = ''
-    def map = [:]
+	def map = [:]
 	def retResult = []
 	def descMap = parseDescriptionAsMap(description)
 	//Image
@@ -200,22 +200,22 @@ def parse(String description) {
 			whichTile = 'test'
 		}
 		if (bodyReturned.contains('CPU=')) {
-            def cpuData=''
-            def data=bodyReturned.eachLine { line ->
-            	if (line.contains('CPU=')) {
-                	cpuData=line
-                }
-            }
-            //log.debug cpuData
+			def cpuData=''
+			def data=bodyReturned.eachLine { line ->
+				if (line.contains('CPU=')) {
+					cpuData=line
+				}
+			}
+			//log.debug cpuData
 			sendEvent(name: "cpuUsage", value: cpuData.toString().replace("=","\n"), unit: "")
 		}
 		if (bodyReturned.contains('Space Used=')) {
-            def spaceUsed=''
-            def data=bodyReturned.eachLine { line ->
-            	if (line.contains('Space Used=')) {
-                	spaceUsed=line
-                }
-            }
+			def spaceUsed=''
+			def data=bodyReturned.eachLine { line ->
+				if (line.contains('Space Used=')) {
+					spaceUsed=line
+				}
+			}
 			sendEvent(name: "spaceUsed", value: spaceUsed.toString().replace("=","\n"), unit: "")
 		}
 		if (bodyReturned.contains('UpTime=')) {
@@ -228,9 +228,9 @@ def parse(String description) {
 			sendEvent(name: "upTime", value: upTime.toString().replace("=","\n"), unit: "")
 		}
 		if (bodyReturned.contains('CPU Temp=')) {
-            def cpuTemp=''
-            def data=bodyReturned.eachLine { line ->
-            	if (line.contains('CPU Temp=')) {
+			def cpuTemp=''
+			def data=bodyReturned.eachLine { line ->
+				if (line.contains('CPU Temp=')) {
 					line=line.replace("\n","")
 					if (cpuTemp.length() == 0) {
 						cpuTemp=line.replace("CPU Temp=","")
@@ -238,8 +238,8 @@ def parse(String description) {
 						cpuTemp=cpuTemp+' = '+line.replace("CPU Temp=","")
 					}
 					//log.debug cpuTemp
-                }
-            }
+				}
+			}
 			sendEvent(name: "cpuTemp", value: "CPU Temp\n"+cpuTemp.toString(), unit: "")
 		}
 	}
@@ -268,7 +268,6 @@ def parse(String description) {
 */
 }
 
-
 def parseDescriptionAsMap(description) {
 	description.split(",").inject([:]) { map, param ->
 	def nameAndValue = param.split(":")
@@ -278,15 +277,15 @@ def parseDescriptionAsMap(description) {
 
 
 private String convertIPtoHex(ipAddress) { 
-    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
-    //log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
-    return hex
+	String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+	//log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
+	return hex
 }
 
 private String convertPortToHex(port) {
 	String hexport = port.toString().format( '%04x', port.toInteger() )
-    //log.debug hexport
-    return hexport
+	//log.debug hexport
+	return hexport
 }
 
 private Integer convertHexToInt(hex) {
@@ -301,7 +300,7 @@ private String convertHexToIP(hex) {
 
 private getHostAddress() {
 	def parts = device.deviceNetworkId.split(":")
-    //log.debug device.deviceNetworkId
+	//log.debug device.deviceNetworkId
 	def ip = convertHexToIP(parts[0])
 	def port = convertHexToInt(parts[1])
 	return ip + ":" + port
