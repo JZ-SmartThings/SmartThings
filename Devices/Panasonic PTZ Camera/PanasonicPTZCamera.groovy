39c5b846ae15601c8d4cba3781022c49a3543975
bimish <mjshutt@gmail.com>
2017-02-01 18:03:24 -0500

    update #2 to use new S3 storage method (#7)

--- Devices/Panasonic PTZ Camera/PanasonicPTZCamera.groovy
+++ Devices/Panasonic PTZ Camera/PanasonicPTZCamera.groovy
@@ -182,10 +182,11 @@ def parse(String description) {
     def map = [:]
 	def retResult = []
 	def descMap = parseDescriptionAsMap(description)
+    def imageKey = descMap["tempImageKey"] ? descMap["tempImageKey"] : descMap["key"]
 	//Image
-	if (descMap["key"]) {
+	if (imageKey) {
 		try {
-			storeTemporaryImage(descMap["key"], getPictureName()) 
+			storeTemporaryImage(imageKey, getPictureName()) 
 		}
 		catch (Exception e) {
 			log.error e
